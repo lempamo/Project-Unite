c879be99287ebb447eaaf688d00cd1d14f3e52b6
Michael <Michael@some.where>
2017-03-21 11:39:34 -0400

    this erks me

--- Project-Unite/Migrations/Configuration.cs
+++ Project-Unite/Migrations/Configuration.cs
@@ -3,6 +3,7 @@ namespace Project_Unite.Migrations
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Diagnostics;
     using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Project_Unite.Models.ApplicationDbContext>
@@ -15,34 +16,7 @@ namespace Project_Unite.Migrations
 
         protected override void Seed(Project_Unite.Models.ApplicationDbContext context)
         {
-
-            var adm = new Models.Role
-            {
-                Id = "administrator",
-                ColorHex = "#FF0000",
-                Description = "These are the admins of the website - This is a persistent group and cannot be deleted.",
-                Priority = context.Roles.Count() + 1,
-                Name = "Administrators"
-            };
-            foreach (var prop in adm.GetType().GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance))
-            {
-                if (prop.Name.StartsWith("Can") && prop.PropertyType == typeof(bool))
-                {
-                    prop.SetValue(adm, true);
-                }
-            }
-            context.Roles.AddOrUpdate(adm);
-            var userRole = new Models.Role
-            {
-                Name = "Users",
-                Id = "user",
-                Description = "This is the default role for all new users. This role's priority may not be modified, and this role may not be deleted.",
-                Priority = 0,
-                ColorHex = "#FFF"
-            };
-            context.Roles.AddOrUpdate(userRole);
-            context.SaveChanges();
-
+            
         }
 
     }
