1d4e39678fd3ff0ad27887768a75a95894be3053
Michael <Michael@some.where>
2017-03-23 19:31:43 -0400

    middle-end for user posts

--- Project-Unite/Controllers/ProfilesController.cs
+++ Project-Unite/Controllers/ProfilesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using Project_Unite.Models;
 
 namespace Project_Unite.Controllers
@@ -24,6 +25,18 @@ namespace Project_Unite.Controllers
                 return new HttpStatusCodeResult(404);
 
             return View(user);
+        }
+
+        public ActionResult PostContent(UserPost model)
+        {
+            var db = new ApplicationDbContext();
+            model.Id = Guid.NewGuid().ToString();
+            model.PostedAt = DateTime.Now;
+            model.UserId = User.Identity.GetUserId();
+            db.UserPosts.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("ViewProfile", "Profiles", new { id = ACL.UserNameRaw(User.Identity.GetUserId()) });
+            
         }
     }
 }
