b7457b5d532506abdae0ab74bfdc45f0f955a526
Michael <Michael@some.where>
2017-04-05 18:45:44 -0400

    Add dev CP index.

---
+++ Project-Unite/Controllers/DeveloperController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_Unite.Controllers
+{
+    [Authorize]
+    public class DeveloperController : Controller
+    {
+        // GET: Developer
+        public ActionResult Index()
+        {
+            ViewBag.Developer = true;
+            return View();
+        }
+    }
+}
