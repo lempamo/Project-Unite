3d7126efd77deadac5f6be00c18e06eb62be49e3
Michael <Michael@some.where>
2017-04-30 10:41:29 -0400

    oauth login api

---
+++ Project-Unite/Controllers/OAuth2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_Unite.Controllers
+{
+    public class OAuth2Controller : Controller
+    {
+        // GET: OAuth2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
