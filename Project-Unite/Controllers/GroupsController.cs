8e6196fd1d6b072ae14451a2119062e3401d1523
Michael <Michael@some.where>
2017-05-07 09:36:02 -0400

    JoinGroup action

--- Project-Unite/Controllers/GroupsController.cs
+++ Project-Unite/Controllers/GroupsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using Project_Unite.Models;
 
 namespace Project_Unite.Controllers
@@ -15,6 +16,20 @@ namespace Project_Unite.Controllers
             var db = new ApplicationDbContext();
             return View(db.Groups);
         }
+
+        [Authorize]
+        public ActionResult JoinGroup(string id)
+        {
+            var db = new ApplicationDbContext();
+            var user = db.Users.FirstOrDefault(x => x.Id == User.Identity.GetUserId());
+            var group = db.Groups.FirstOrDefault(x => x.Id == id);
+            if (group == null)
+                return new HttpStatusCodeResult(404);
+            user.GroupId = id;
+            db.SaveChanges();
+            return RedirectToAction("ViewGroup", "Groups", new { id = id });
+        }
+
 
         [Authorize]
         public ActionResult ViewGroup(string id)
