ca5954e7d2b9c2040f6f421816778d8203161563
Michael <Michael@some.where>
2017-05-07 10:04:12 -0400

    Create group page

--- Project-Unite/Controllers/GroupsController.cs
+++ Project-Unite/Controllers/GroupsController.cs
@@ -18,6 +18,71 @@ namespace Project_Unite.Controllers
         }
 
         [Authorize]
+        public ActionResult CreateGroup()
+        {
+            //NOPE. I'm not circumming to the ways of Victor Tran. CURLY BRACES ON THEIR OWN LINE.
+            var model = new GroupViewModel();
+            return View(model);
+        }
+
+        private bool ValidateHex(string hex)
+        {
+            if (!(hex.Length == 3 || hex.Length == 6))
+                return false;
+            string hexallowed = "0123456789abcdef";
+            foreach(var c in hex.ToLower().ToCharArray())
+            {
+                if (!hexallowed.Contains(c))
+                    return false;
+            }
+            return true;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateGroup(GroupViewModel model)
+        {
+            var result = ValidateHex(model.BannerColorHex);
+
+            if(result == false)
+            {
+                ModelState.AddModelError("BannerColorHex", new Exception("Invalid hexadecimal color code."));
+            }
+
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var db = new ApplicationDbContext();
+            var group = new Group();
+            group.Id = Guid.NewGuid().ToString();
+            group.Name = model.Name;
+            group.ShortName = model.ShortName;
+            group.Description = model.Description;
+            switch (model.Publicity)
+            {
+                case "public":
+                    group.Publicity = 0;
+                    break;
+                case "publici":
+                    group.Publicity = 1;
+                    break;
+                case "private":
+                    group.Publicity = 2;
+                    break;
+                case "privatei":
+                    group.Publicity = 3;
+                    break;
+            }
+            group.RawReputation = 0.00;
+            group.BannerColorHex = model.BannerColorHex;
+
+            db.Groups.Add(group);
+            db.SaveChanges();
+            return RedirectToAction("JoinGroup", "Groups", new { id = group.Id });
+        }
+
+        [Authorize]
         public ActionResult JoinGroup(string id)
         {
             var db = new ApplicationDbContext();
--- Project-Unite/Models/Group.cs
+++ Project-Unite/Models/Group.cs
@@ -1,19 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Project_Unite.Models
 {
-    public class Group
+    public class GroupViewModel
     {
-        public string Id { get; set; }
+        public List<SelectListItem> Publicities
+        {
+            get
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem { Value="public", Text="Public"},
+                    new SelectListItem { Value="publici", Text="Public (Invite Only)"},
+                    new SelectListItem { Value="private", Text="Private"},
+                    new SelectListItem { Value="privatei", Text="Private (Invite Only)" }
+                };
+            }
+        }
+
+        [Required]
+        [MaxLength(25, ErrorMessage = "Your group's name must have a maximum of 25 characters in it.")]
+        [MinLength(5, ErrorMessage = "You must set a name with at least 5 characters in it.")]
         public string Name { get; set; }
-        public int Publicity { get; set; }
+
+        [Required]
+        public string Publicity { get; set; }
+
+        [Required]
+        [MaxLength(6, ErrorMessage = "Hexadecimal color values can only have 6 or less digits.")]
+        [MinLength(3, ErrorMessage = "Hexadecimal color values must have at least 3 digits.")]
         public string BannerColorHex { get; set; }
+
+        [Required]
+        [AllowHtml]
         public string Description { get; set; }
+
+        [Required]
+        [MaxLength(4, ErrorMessage = "Your Short Name can only have 4 characters. Think of it like an acronym.")]
         public string ShortName { get; set; }
 
+    }
+
+
+    public class Group
+    {
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(25, ErrorMessage ="Your group's name must have a maximum of 25 characters in it.")]
+        [MinLength(5, ErrorMessage ="You must set a name with at least 5 characters in it.")]
+        public string Name { get; set; }
+
+        [Required]
+        public int Publicity { get; set; }
+
+        [Required]
+        [MaxLength(6, ErrorMessage ="Hexadecimal color values can only have 6 or less digits.")]
+        [MinLength(3, ErrorMessage ="Hexadecimal color values must have at least 3 digits.")]
+        public string BannerColorHex { get; set; }
+
+        [Required]
+        [AllowHtml]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(4, ErrorMessage ="Your Short Name can only have 4 characters. Think of it like an acronym.")]
+        public string ShortName { get; set; }
+
+        [Required]
         public double RawReputation { get; set; }
 
         public ApplicationUser[] Users
