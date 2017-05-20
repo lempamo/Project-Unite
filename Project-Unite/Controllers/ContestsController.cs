b0c200196e5506b456895f9ee0c4c8dcd547fdb7
Michael <Michael@some.where>
2017-05-20 19:42:32 -0400

    viewcontest page

--- Project-Unite/Controllers/ContestsController.cs
+++ Project-Unite/Controllers/ContestsController.cs
@@ -7,13 +7,24 @@ using Project_Unite.Models;
 
 namespace Project_Unite.Controllers
 {
+    [Authorize]
     public class ContestsController : Controller
     {
+        [AllowAnonymous]
         // GET: Contests
         public ActionResult Index()
         {
             var model = new ApplicationDbContext().Contests.ToArray();
             return View(model);
+        }
+
+        public ActionResult ViewContest(string id)
+        {
+            var db = new ApplicationDbContext();
+            var c = db.Contests.FirstOrDefault(x => x.Id == id);
+            if (c == null)
+                return new HttpStatusCodeResult(404);
+            return View(c);
         }
     }
 }
