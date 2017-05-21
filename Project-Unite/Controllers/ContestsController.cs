e37b3223dbcd7957bbe113c5cb9a5698205fb722
Michael <Michael@some.where>
2017-05-20 20:06:42 -0400

    create contest page

--- Project-Unite/Controllers/ContestsController.cs
+++ Project-Unite/Controllers/ContestsController.cs
@@ -26,5 +26,58 @@ namespace Project_Unite.Controllers
                 return new HttpStatusCodeResult(404);
             return View(c);
         }
+
+        [RequiresAdmin]
+        public ActionResult CloseContest(string id)
+        {
+            var db = new ApplicationDbContext();
+            var c = db.Contests.FirstOrDefault(x => x.Id == id);
+            if (c == null)
+                return new HttpStatusCodeResult(404);
+            c.EndsAt = DateTime.Now;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        [RequiresAdmin]
+        public ActionResult CreateContest()
+        {
+            var model = new CreateContestViewModel();
+            return View(model);
+        }
+
+        [RequiresAdmin]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateContest(CreateContestViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var db = new ApplicationDbContext();
+
+            string allowed = "abcdefghijklmnopqrstuvwxyz_0123456789";
+
+            var c = new Contest();
+
+            c.Name = model.Name;
+            c.Description = model.Description;
+            c.StartedAt = DateTime.Now;
+            c.EndsAt = model.EndDate;
+            c.VideoId = model.VideoId;
+            string id = c.Name.ToLower() + "_" + db.Contests.Count();
+            foreach (char ch in id.ToCharArray())
+                if (!allowed.Contains(ch))
+                    id = id.Replace(ch, '_');
+            c.Id = id;
+            c.CodepointReward1st = model.GoldReward;
+            c.CodepointReward2nd = model.SilverReward;
+            c.CodepointReward3rd = model.BronzeReward;
+            db.Contests.Add(c);
+            db.SaveChanges();
+
+            return RedirectToAction("ViewContest", new { id = c.Id });
+        }
     }
 }
--- Project-Unite/Models/ContestModels.cs
+++ Project-Unite/Models/ContestModels.cs
@@ -2,9 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Project_Unite.Models
 {
+    public class CreateContestViewModel
+    {
+        [Required(AllowEmptyStrings =false, ErrorMessage ="You must name your contest!")]
+        [MinLength(5, ErrorMessage ="Your contest name must contain at least 5 characters.")]
+        [MaxLength(35, ErrorMessage ="Your contest's name must not have more than 35 characters!")]
+        public string Name { get; set; }
+
+        public string ContestId { get; set; }
+
+        public List<SelectListItem> Contests
+        {
+            get
+            {
+                var db = new ApplicationDbContext();
+                var list = new List<SelectListItem>();
+                foreach (var c in db.Contests.Where(x => x.IsEnded == false).OrderByDescending(x => x.StartedAt).ToArray())
+                {
+                    list.Add(new SelectListItem
+                    {
+                        Value = c.Id,
+                        Text = c.Name
+                    });
+                }
+                return list;
+            }
+        }
+
+        [AllowHtml]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please describe your contest!")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage ="Please set an end date for the contest.")]
+        public DateTime EndDate { get; set; }
+
+        [Required(ErrorMessage ="Please specify a Codepoint reward for the gold winner.")]
+        public long GoldReward { get; set; }
+
+        [Required(ErrorMessage = "Please specify a Codepoint reward for the silver winner.")]
+        public long SilverReward { get; set; }
+
+        [Required(ErrorMessage = "Please specify a Codepoint reward for the bronze winner.")]
+        public long BronzeReward { get; set; }
+
+
+        public string VideoId { get; set; }
+    }
+
     public class Contest
     {
         public string Id { get; set; }
