1dcba9a68839ac0eac372030b20d9c0c8f2f8e0e
Michael <Michael@some.where>
2017-05-31 08:21:08 -0400

    Make a simple mod to the download notification

--- Project-Unite/Controllers/DeveloperController.cs
+++ Project-Unite/Controllers/DeveloperController.cs
@@ -143,13 +143,15 @@ namespace Project_Unite.Controllers
                 model.Screenshot.SaveAs(mapped_dir);
             }
 
+            string downloadIsStable = (download.IsStable) ? "Yes" : "No";
+
             //Now we just save to the database...
             db.Downloads.Add(download);
 
             NotificationDaemon.ScreamToDiscord("New release: " + download.Name, $@"A new release of ShiftOS has been made!
 
 Release name: {download.Name}
-Stable: {download.IsStable}
+Stable: {downloadIsStable}
 Released on: {download.PostDate}", Url.Action("ViewRelease", "Downloads", new { id = download.Id }));
 
             db.SaveChanges();
